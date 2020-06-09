74388d9c821f98848e53d25d36cbb0e379760182
stevenli2020 <stevenli2020@github.com>
2020-06-09 07:37:50 +0000

    sdp server codes

--- sdp_controller/app/sdp_ctrl.py
+++ sdp_controller/app/sdp_ctrl.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import json,sys,socket,threading,subprocess,time,os.path,atexit,os,glob
+import json,sys,socket,threading,subprocess,time,os.path,atexit,os,glob,base64,traceback
+import paramiko
+from binascii import hexlify
 from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
+from paramiko.py3compat import b, u, decodebytes
 
--- sdp_controller/app/spa_service.py
+++ sdp_controller/app/spa_service.py
@@ -1,24 +1,62 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import json,sys,socket,threading,subprocess,time,os.path,atexit,os,glob
+import json,sys,socket,threading,subprocess,time,os.path,atexit,os,glob,shutil
 from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 
-def At_Exit_Cleanup_Sessions():
-	filelist = glob.glob(os.path.join("sessions", "*"))
-	if filelist == []:
-		print("\nBye")
-		return
-	print("Clean up session files:")
-	for f in filelist:
-		os.remove(f)
-		print(" > Deleted session -",f)
+def At_Exit():
+	Cleanup_Sessions()
+	Restore_Firewall()
 	print("\nBye")
 
-atexit.register(At_Exit_Cleanup_Sessions) 
+atexit.register(At_Exit) 
+
+def Disconnect_User(UID):
+	print("Disconnect user:",UID)
+	try:
+	#pkill -KILL -u [UID]
+		subprocess.check_output(["pkill", "-KILL", "-u", UID])
+	except:
+		pass
+	Delete_User(UID)
+	Delete_Session(UID)
 	
-def Create_SSL_Session(UID, CONF, IP):
+	
+def Delete_User(UID):
+	print("Delete user:",UID)
+	try:
+		#userdel -f [UID]
+		subprocess.check_output(["userdel", "-f", UID])
+		shutil.rmtree("/home/"+UID)
+		print(" > Deleted session user -",UID)
+	except:
+		pass
+	
+def Delete_Session(UID):
+	SESSION_ID = UID[0:16]
+	print("Delete session:",SESSION_ID)
+	try:
+		os.remove("/app/sessions/"+SESSION_ID)
+		print(" > Deleted session file -",SESSION_ID)	
+	except:
+		pass
+	
+def Cleanup_Sessions():
+	userlist = glob.glob(os.path.join("/home", "*"))
+	if userlist != []:
+		print("Cleaning up session users:")
+		for d in userlist:
+			USERNAME = d.replace("/home/","")
+			Disconnect_User(USERNAME)
+
+
+	
+def Restore_Firewall():
+	#iptables -F INPUT
+	subprocess.check_output(["iptables", "-F", "INPUT"])
+		
+def Create_SSL_Session(UID, OTP, CONF, IP):
 	global SERVER_SEED
 	print("Create SSL session configuration for", UID)
 	SESSION = {}
@@ -31,6 +69,12 @@ def Create_SSL_Session(UID, CONF, IP):
 	with open('sessions/'+UID,'w+') as f:
 		f.write(json.dumps(SESSION,indent=2))
 		f.truncate()
+	# add session user: useradd -m username
+	print("Create session user", UID+OTP)
+	subprocess.check_output(["useradd", "-m", UID+OTP])
+	os.makedirs('/home/'+UID+OTP+'/.ssh') 
+	with open('/home/'+UID+OTP+'/.ssh/authorized_keys','w+') as f:
+		f.write(CONF['RSA_PUB_KEY'])
 
 def Sync_Counter(UID,CTR):
 	with open('users','r+') as f:
@@ -40,13 +84,13 @@ def Sync_Counter(UID,CTR):
 		f.write(json.dumps(USERS,indent=2))
 		f.truncate()
 
-def Local_Firewall_P60022_Open_10s(IP):
-	# iptables -I INPUT -p tcp -s 1.1.1.1 --dport 60022 -j ACCEPT
-	print("Firewall add tcp/60022 for", IP)
-	subprocess.check_output(["iptables", "-I", "INPUT", "1", "-p", "tcp", "-s", IP, "--dport", "60022", "-j", "ACCEPT"])
+def Local_Firewall_P22_Open_10s(IP):
+	# iptables -I INPUT -p tcp -s 1.1.1.1 --dport 22 -j ACCEPT
+	print("Firewall add tcp/22 for", IP)
+	subprocess.check_output(["iptables", "-I", "INPUT", "1", "-p", "tcp", "-s", IP, "--dport", "22", "-j", "ACCEPT"])
 	time.sleep(10)
-	subprocess.check_output(["iptables", "-D", "INPUT", "-p", "tcp", "-s", IP, "--dport", "60022", "-j", "ACCEPT"])
-	print("Firewall delete tcp/60022 for", IP)
+	subprocess.check_output(["iptables", "-D", "INPUT", "-p", "tcp", "-s", IP, "--dport", "22", "-j", "ACCEPT"])
+	print("Firewall delete tcp/22 for", IP)
 		
 def Validate_SPA(DATA, ADDR):
 	global SERVER_SEED,SERVER_SHARED_IV,SERVER_SHARED_KEY
@@ -83,8 +127,8 @@ def Validate_SPA(DATA, ADDR):
 		SERVER_OTP = bytes.fromhex(HASH.hexdigest()[:16])		
 	#Sync_Counter(REQ_UID_HEX, USERS[REQ_UID_HEX]["SPA_CTR"]+i)
 	print("SPA from %s OK" %(REQ_UID_HEX))
-	Create_SSL_Session(REQ_UID_HEX, USERS[REQ_UID_HEX], ADDR[0])
-	Local_Firewall_P60022_Open_10s(ADDR[0])
+	Create_SSL_Session(REQ_UID_HEX, REQ_OTP.hex(), USERS[REQ_UID_HEX], ADDR[0])
+	Local_Firewall_P22_Open_10s(ADDR[0])
 
 def Initialize_Firewall_Rulles():
 	#iptables -P INPUT DROP 
@@ -98,14 +142,26 @@ def Initialize_Firewall_Rulles():
 	#iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 	subprocess.check_output(["iptables", "-A", "INPUT", "-m", "conntrack", "--ctstate", "ESTABLISHED,RELATED", "-j", "ACCEPT"])
 
-	
-	
-	
-	
-	
+def Monitor_Sessions(INTERVAL):
+	while True:
+		time.sleep(5)
+		userlist = glob.glob(os.path.join("/home", "*"))
+		if userlist == []:
+			continue
+		WHO = subprocess.check_output(["who", "-u"]).decode("utf-8")
+		for d in userlist:
+			if time.time()-os.path.getatime(d) < INTERVAL:
+				continue
+			USERNAME = d.replace("/home/","")
+			if USERNAME not in WHO:
+				Delete_User(USERNAME)
+				Delete_Session(USERNAME[:16])
+
 # Main program	
 print("Initialize Firewal rulls")	
 Initialize_Firewall_Rulles()
+print("Initialize Sessions")
+Cleanup_Sessions()
 print("Initialize server configurations")
 with open('config','r') as f:
     CONFIG = json.loads(f.read())
@@ -113,6 +169,8 @@ SPA_PORT = int(CONFIG['SPA_PORT'])
 SERVER_SHARED_KEY = bytes.fromhex(CONFIG['SERVER_SHARED_KEY'])
 SERVER_SHARED_IV = bytes.fromhex(CONFIG['SERVER_SHARED_IV'])
 SERVER_SEED = bytes.fromhex(CONFIG['SERVER_SEED'])
+print("Start session watchdog")
+threading.Thread(target=Monitor_Sessions,args=(5,)).start()
 print("Start UDP server")
 SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 SOCKET.bind(("", SPA_PORT))
