b92a1ec16905a3761e0294b646a0fa52b09a2cdf
stevenli2020 <stevenli2020@github.com>
2020-06-16 09:19:36 +0000

    updates

---
+++ client/app/ssh_loop.ps1
@@ -0,0 +1,11 @@
+$gateway=$args[0]
+while([IO.File]::ReadAllText("C:\docker\sdp\client\app\e").trim() -eq 0){
+	$session_id = [IO.File]::ReadAllText("C:\docker\sdp\client\app\session_id").trim()
+	if($session_id -ne "") {
+		echo "Session ID received, establish TLS tunnel"
+		ssh -D 50000 -N -C -q $session_id@$gateway -p50022
+		echo "TLS tunnel disconnected, clear session id"
+		Clear-Content C:\docker\sdp\client\app\session_id
+	}
+	Start-Sleep -s 1
+}
